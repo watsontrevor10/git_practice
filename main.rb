fb41afe552907d73f55bf598a7cf813cc677be98
Trevor Watson <watsontrevor10@gmail.com>
2019-11-07 11:21:21 -0700

    added Gemfile and colorize gem and added color to puts text

--- main.rb
+++ main.rb
@@ -1,20 +1,24 @@
+require "pry"
+require "colorize"
+
 def puts_git(cmd)
   puts `git #{cmd} - h`
 end
 
 def menu
-  puts "1: Enter git command"
-  puts "2: Exit"
+  puts "-- Main Menu --".colorize(:cyan)
+  puts "1: Enter git command".colorize(:cyan)
+  puts "2: Exit".colorize(:cyan)
   
   case gets.to_i
   when 1
-    puts "Enter git command:"
+    puts "Enter git command:".colorize(:green)
     puts_git(gets.strip)
     menu
   when 2
     exit
   else 
-    puts "Invalid choice"
+    puts "Invalid choice".colorize(:red)
     menu
   end
 end
